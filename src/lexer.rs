80abe9b8194d27516e6c9c4adfabf99f39fd85f0
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2025-03-25 23:04:17 +0100

    Add test --built-in command

--- src/lexer.rs
+++ src/lexer.rs
@@ -36,18 +36,96 @@ impl Clone for Word {
     }
 }
 
-pub fn parse_line_into_words(file: String, row: u32, line: &str) -> Vec<Word> {
-    let mut result = vec![];
-    let mut pos = find_char(line, 0, |x| !x.is_whitespace());
-    while pos < line.len() {
-        let end_pos = find_char(&line, pos + 1, |x| x.is_whitespace());
-        result.push(Word {
-            file: file.clone(),
-            row,
-            col: pos as u32,
-            txt: line[pos..end_pos].to_string(),
-        });
-        pos = find_char(line, end_pos + 1, |x| !x.is_whitespace());
+pub fn parse_lines_into_words(file: String, source_lines: Vec<String>) -> Vec<Word> {
+    let mut lines: Vec<&str> = source_lines.iter().map(|x| x.as_str()).collect();
+    lines.reverse();
+    let mut result: Vec<Word> = vec![];
+    let mut row: i64 = -1;
+    while !lines.is_empty() {
+        let mut line = lines.pop().unwrap();
+        row += 1;
+        let mut pos = find_char(&line, 0, |x| !x.is_whitespace());
+        while pos < line.len() {
+            if line.chars().next().unwrap() == '\'' {
+                let end_pos = find_char(&line, pos + 2, |x| x == '\'');
+                if end_pos >= line.len()
+                    || (line.chars().nth(pos + 1).unwrap() != '\\' && end_pos - pos >= 3)
+                    || (line.chars().nth(pos + 1).unwrap() == '\\' && end_pos - pos >= 4)
+                {
+                    eprintln!(
+                        "{}:{}:{}: ERROR: Encountered invalid character literal",
+                        file,
+                        row + 1,
+                        pos + 1
+                    );
+                    std::process::exit(1);
+                }
+                let val = line[pos..end_pos].chars().nth(0).unwrap();
+                result.push(Word {
+                    file: file.clone(),
+                    row: row as u32,
+                    col: pos as u32,
+                    txt: (val as u32).to_string(),
+                });
+                pos = find_char(&line, end_pos + 1, |x| !x.is_whitespace());
+                continue;
+            } else if line.chars().next().unwrap() == '"' {
+                let end_pos = find_char(&line, pos + 1, |x| x == '"');
+                if end_pos >= line.len() {
+                    let start_row = row;
+                    let start_col = pos;
+                    let mut string_buffer = vec![&line[pos..]];
+                    pos = 0;
+                    while !lines.is_empty() {
+                        line = lines.pop().unwrap();
+                        row += 1;
+                        let end_pos = find_char(&line, pos + 1, |x| x == '"');
+                        if end_pos >= line.len() {
+                            string_buffer.push(line);
+                        } else {
+                            string_buffer.push(&line[0..(end_pos + 1)]);
+                            result.push(Word {
+                                file: file.clone(),
+                                row: start_row as u32,
+                                col: start_col as u32,
+                                txt: string_buffer.join("\n"),
+                            });
+                            pos = find_char(line, end_pos + 2, |x| !x.is_whitespace());
+                            break;
+                        }
+                    }
+                } else {
+                    result.push(Word {
+                        file: file.clone(),
+                        row: row as u32,
+                        col: pos as u32,
+                        txt: line[pos..end_pos + 1].to_string(),
+                    });
+                    pos = find_char(line, end_pos + 2, |x| !x.is_whitespace());
+                    continue;
+                }
+            }
+            let splitted: Vec<&str> = line.split("//").collect();
+            if splitted.is_empty() {
+                break;
+            }
+            line = splitted.first().unwrap();
+            let end_pos = find_char(&line, pos + 1, |x| x.is_whitespace());
+            if end_pos > line.len() {
+                break;
+            }
+            let token_text = line[pos..end_pos].to_string();
+            if token_text.starts_with("//") {
+                break;
+            }
+            result.push(Word {
+                file: file.clone(),
+                row: row as u32,
+                col: pos as u32,
+                txt: token_text,
+            });
+            pos = find_char(line, end_pos + 1, |x| !x.is_whitespace());
+        }
     }
     result
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,90 @@
+use crate::tokenizer::Token;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
 mod checker;
 mod lexer;
 mod simulator;
 mod tokenizer;
 
 fn main() {
-    let words = lexer::parse_line_into_words(String::from("<generated>"), 0, "1 2 + dump");
+    let mut args: Vec<String> = std::env::args().collect();
+    if args.len() < 3 {
+        usage(&args[0]);
+        std::process::exit(0);
+    }
+
+    let last_arg = args.pop().unwrap();
+    match args[1].as_str() {
+        // "simulate" => match read_file_contents(&last_arg) {
+        //     Ok(lines) => {
+        //         let program = compile_program(last_arg, lines);
+        //         simulator::simulate_tokens(program);
+        //         std::process::exit(0);
+        //     }
+        //     Err(err) => {
+        //         eprintln!("ERROR: Could not load program {}!", last_arg);
+        //         eprintln!("ERROR: {}", err);
+        //     }
+        // },
+        "test" => {
+            if "--built-in" == last_arg {
+                run_builtin_test();
+                std::process::exit(0);
+            } else {
+                match read_file_contents(&last_arg) {
+                    Ok(lines) => {
+                        let program = compile_program(last_arg, lines);
+                        simulator::simulate_tokens(program);
+                        std::process::exit(0);
+                    }
+                    Err(err) => {
+                        eprintln!("ERROR: Could not load program {}!", last_arg);
+                        eprintln!("ERROR: {}", err);
+                    }
+                }
+            }
+        }
+        _ => {
+            usage(&args[0]);
+            std::process::exit(0);
+        }
+    };
+}
+
+fn usage(self_path: &str) {
+    println!("Usage: {} <COMMAND> [OPTIONS] <file_path>", self_path);
+    println!("Available commands:");
+    // println!("  simulate        Interpret and simulate the given program");
+    // println!("    Available options:");
+    println!("  test            Interpret and test the given program");
+    println!("    Available options:");
+    println!("      --built-in  Run the built-in test program. Does not need a file path.");
+}
+
+fn read_file_contents(path: &str) -> io::Result<Vec<String>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let mut lines = Vec::new();
+    for line in reader.lines() {
+        let line = line?;
+        lines.push(line);
+    }
+    Ok(lines)
+}
+
+fn compile_program(file: String, lines: Vec<String>) -> Vec<Token> {
+    let words = lexer::parse_lines_into_words(file, lines);
     let tokens = tokenizer::parse_words_into_tokens(words);
+    checker::check_types(&tokens, 0);
+    tokens
+}
+
+fn run_builtin_test() {
+    let tokens = compile_program(
+        String::from("<generated>"),
+        vec![String::from("1 2 + dump")],
+    );
     checker::check_types(&tokens, 0);
     simulator::simulate_tokens(tokens);
 }
