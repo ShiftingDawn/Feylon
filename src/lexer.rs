b8bfbf30ee31262347ccac22c6c33f4966f5cfc8
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2025-03-29 20:58:09 +0100

    Split lexer char and string parsing code

--- src/lexer.rs
+++ src/lexer.rs
@@ -35,89 +35,113 @@ impl Clone for Word {
     }
 }
 
+struct LexerContext<'a> {
+    file: String,
+    lines: Vec<&'a str>,
+    pos: usize,
+    row: usize,
+    result: Vec<Word>,
+    line: &'a str,
+}
+
 pub fn parse_lines_into_words(file: String, source_lines: Vec<String>) -> Vec<Word> {
-    let mut lines: Vec<&str> = source_lines.iter().map(|x| x.as_str()).collect();
-    lines.reverse();
-    let mut result: Vec<Word> = vec![];
-    let mut row: i64 = -1;
-    while !lines.is_empty() {
-        let mut line = lines.pop().unwrap();
-        row += 1;
-        let mut pos = find_char(&line, 0, |x| !x.is_whitespace());
-        while pos < line.len() {
-            if line.chars().nth(pos).unwrap() == '\'' {
-                let end_pos = find_char(&line, pos + 2, |x| x == '\'');
-                if end_pos >= line.len() || (line.chars().nth(pos + 1).unwrap() != '\\' && end_pos - pos >= 3) || (line.chars().nth(pos + 1).unwrap() == '\\' && end_pos - pos >= 4)
-                {
-                    eprintln!("{}:{}:{}: ERROR: Encountered invalid character literal", file, row + 1, pos + 1);
-                    std::process::exit(1);
-                }
-                let val = line[pos..end_pos].chars().nth(0).unwrap();
-                result.push(Word {
-                    file: file.clone(),
-                    row: row as u32,
-                    col: pos as u32,
-                    txt: (val as u32).to_string(),
-                });
-                pos = find_char(&line, end_pos + 1, |x| !x.is_whitespace());
+    let mut ctx = LexerContext {
+        file,
+        lines: source_lines.iter().map(|x| x.as_str()).collect(),
+        pos: 0,
+        row: 0,
+        result: vec![],
+        line: "",
+    };
+    ctx.lines.reverse();
+    while !ctx.lines.is_empty() {
+        ctx.line = ctx.lines.pop().unwrap();
+        ctx.row += 1;
+        ctx.pos = find_char(&ctx.line, 0, |x| !x.is_whitespace());
+        while ctx.pos < ctx.line.len() {
+            if ctx.line.chars().nth(ctx.pos).unwrap() == '\'' {
+                parse_char(&mut ctx);
                 continue;
-            } else if line.chars().nth(pos).unwrap() == '"' {
-                let end_pos = find_char(&line, pos + 1, |x| x == '"');
-                if end_pos >= line.len() {
-                    let start_row = row;
-                    let start_col = pos;
-                    let mut string_buffer = vec![&line[pos..]];
-                    pos = 0;
-                    while !lines.is_empty() {
-                        line = lines.pop().unwrap();
-                        row += 1;
-                        let end_pos = find_char(&line, pos + 1, |x| x == '"');
-                        if end_pos >= line.len() {
-                            string_buffer.push(line);
-                        } else {
-                            string_buffer.push(&line[0..(end_pos + 1)]);
-                            result.push(Word {
-                                file: file.clone(),
-                                row: start_row as u32,
-                                col: start_col as u32,
-                                txt: string_buffer.join("\n"),
-                            });
-                            pos = find_char(&line, end_pos + 2, |x| !x.is_whitespace());
-                            break;
-                        }
-                    }
-                } else {
-                    result.push(Word {
-                        file: file.clone(),
-                        row: row as u32,
-                        col: pos as u32,
-                        txt: line[pos..end_pos + 1].to_string(),
-                    });
-                    pos = find_char(&line, end_pos + 2, |x| !x.is_whitespace());
-                    continue;
-                }
+            } else if ctx.line.chars().nth(ctx.pos).unwrap() == '"' {
+                parse_string(&mut ctx);
+                continue;
             }
-            let splitted: Vec<&str> = line.split("//").collect();
+            let splitted: Vec<&str> = ctx.line.split("//").collect();
             if splitted.is_empty() {
                 break;
             }
-            line = splitted.first().unwrap();
-            let end_pos = find_char(&line, pos + 1, |x| x.is_whitespace());
-            if end_pos > line.len() {
+            ctx.line = splitted.first().unwrap();
+            let end_pos = find_char(&ctx.line, ctx.pos + 1, |x| x.is_whitespace());
+            if end_pos > ctx.line.len() {
                 break;
             }
-            let token_text = line[pos..end_pos].to_string();
+            let token_text = ctx.line[ctx.pos..end_pos].to_string();
             if token_text.starts_with("//") {
                 break;
             }
-            result.push(Word {
-                file: file.clone(),
-                row: row as u32,
-                col: pos as u32,
+            ctx.result.push(Word {
+                file: ctx.file.clone(),
+                row: ctx.row as u32,
+                col: ctx.pos as u32,
                 txt: token_text,
             });
-            pos = find_char(&line, end_pos + 1, |x| !x.is_whitespace());
+            ctx.pos = find_char(&ctx.line, end_pos + 1, |x| !x.is_whitespace());
         }
     }
-    result
+    ctx.result
+}
+
+fn parse_char(ctx: &mut LexerContext) {
+    let end_pos = find_char(&ctx.line, ctx.pos + 2, |x| x == '\'');
+    if end_pos >= ctx.line.len()
+        || (ctx.line.chars().nth(ctx.pos + 1).unwrap() != '\\' && end_pos - ctx.pos >= 3)
+        || (ctx.line.chars().nth(ctx.pos + 1).unwrap() == '\\' && end_pos - ctx.pos >= 4)
+    {
+        eprintln!("{}:{}:{}: ERROR: Encountered invalid character literal", ctx.file, ctx.row + 1, ctx.pos + 1);
+        std::process::exit(1);
+    }
+    let val = ctx.line[ctx.pos..end_pos].chars().nth(0).unwrap();
+    ctx.result.push(Word {
+        file: ctx.file.clone(),
+        row: ctx.row as u32,
+        col: ctx.pos as u32,
+        txt: (val as u32).to_string(),
+    });
+    ctx.pos = find_char(&ctx.line, end_pos + 1, |x| !x.is_whitespace());
+}
+
+fn parse_string(ctx: &mut LexerContext) {
+    let end_pos = find_char(&ctx.line, ctx.pos + 1, |x| x == '"');
+    if end_pos >= ctx.line.len() {
+        let start_row = ctx.row;
+        let start_col = ctx.pos;
+        let mut string_buffer = vec![&ctx.line[ctx.pos..]];
+        ctx.pos = 0;
+        while !ctx.lines.is_empty() {
+            ctx.line = ctx.lines.pop().unwrap();
+            ctx.row += 1;
+            let end_pos = find_char(&ctx.line, ctx.pos + 1, |x| x == '"');
+            if end_pos >= ctx.line.len() {
+                string_buffer.push(ctx.line);
+            } else {
+                string_buffer.push(&ctx.line[0..(end_pos + 1)]);
+                ctx.result.push(Word {
+                    file: ctx.file.clone(),
+                    row: start_row as u32,
+                    col: start_col as u32,
+                    txt: string_buffer.join("\n"),
+                });
+                ctx.pos = find_char(&ctx.line, end_pos + 2, |x| !x.is_whitespace());
+                break;
+            }
+        }
+    } else {
+        ctx.result.push(Word {
+            file: ctx.file.clone(),
+            row: ctx.row as u32,
+            col: ctx.pos as u32,
+            txt: ctx.line[ctx.pos..end_pos + 1].to_string(),
+        });
+        ctx.pos = find_char(&ctx.line, end_pos + 2, |x| !x.is_whitespace());
+    }
 }
